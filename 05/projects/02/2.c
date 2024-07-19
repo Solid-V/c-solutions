05362ba38d70ff331541659d9fa42f104adfe823
V <107913728+Solid-V@users.noreply.github.com>
2024-07-19 21:28:51 +0530

    Update 2.c
    Instead of hour < 11 changed it to hour <= 11 because when you input 11 as an hour it says hour is more than 11 thus it gets subtracted from 12 giving us a negative value or -1. 
    
    changed %d to %.2d to display an extra zero.

--- 05/projects/02/2.c
+++ 05/projects/02/2.c
@@ -7,11 +7,11 @@ int main(void) {
     printf("Enter a 24-hour time: ");
     scanf("%d :%d", &hour, &minute);
 
-    if (hour < 11)
-        printf("Equivalent 12-hour time: %d:%d AM\n", 
+    if (hour <= 11)
+        printf("Equivalent 12-hour time: %.2d:%.2d AM\n", 
                 hour == 0 ? 12 : hour, minute);
     else 
-        printf("Equivalent 12-hour time: %d:%d PM\n", 
+        printf("Equivalent 12-hour time: %.2d:%.2d PM\n", 
                hour == 12 ? 12 : hour - 12, minute);
 
     return 0;
